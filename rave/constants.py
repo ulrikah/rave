feaacfe6e7fce8ea1bbf9b8117fa4869f003ffab
ulrikah <ulrikhalmoy@gmail.com>
2021-02-26 14:09:54 +0100

    Use absolute paths in constants as a temporary fix
    rllib fails to load relative paths for some reason, although I validate that the paths are valid

--- rave/constants.py
+++ rave/constants.py
@@ -1,12 +1,24 @@
 import os
 
+"""
+NOTE: 
+For some reason, ray fails when paths are relative. 
+The current fix is to append the project ROOT path to all other dir paths, 
+but there has to be something better
+"""
+PROJECT_ROOT = os.getcwd()
+
+AUDIO_INPUT_DIR = os.path.join(PROJECT_ROOT, "rave/input_audio")
+AUDIO_OUTPUT_DIR = os.path.join(PROJECT_ROOT, "rave/bounces")
+CSD_DIR = os.path.join(PROJECT_ROOT, "rave/csd")
+EFFECT_TEMPLATE_DIR = os.path.join(PROJECT_ROOT, "rave/effects")
+ANALYSER_DIR = os.path.join(PROJECT_ROOT, "rave/feature_extractors")
+RAY_RESULTS_DIR = os.path.join(PROJECT_ROOT, "rave/ray_results")
+
 # sound
 LIVE = "adc"
 NO_SOUND = "--nosound"
 DAC = "dac"
-AUDIO_INPUT_DIR = "rave/input_audio"
-AUDIO_OUTPUT_DIR = "rave/bounces"
-CSD_DIR = "rave/csd"
 SAMPLE_RATE = 44100
 KSMPS = 64
 WAVE_FILE_FLAG = "-W"  # write audio output as WAVE file instead of AIFF
@@ -14,20 +26,15 @@ CSD_JINJA_SUFFIX = ".csd.jinja2"
 CSD_JINJA_GLOBALS_SUFFIX = f".globals{CSD_JINJA_SUFFIX}"
 
 # effects
-EFFECT_TEMPLATE_DIR = "rave/effects"
 EFFECT_BASE = f"base{CSD_JINJA_SUFFIX}"
 
 # analyser
-ANALYSER_DIR = "rave/feature_extractors"
 ANALYSER_BASE = f"base_analyser{CSD_JINJA_SUFFIX}"
 
 
-# ray
-RAY_RESULTS_DIR = "rave/ray_results"
-
-
-assert os.path.isdir(AUDIO_INPUT_DIR)
-assert os.path.isdir(AUDIO_OUTPUT_DIR)
-assert os.path.isdir(EFFECT_TEMPLATE_DIR)
-assert os.path.isdir(ANALYSER_DIR)
-assert os.path.isdir(RAY_RESULTS_DIR)
+if __name__ == "__main__":
+    assert os.path.isdir(AUDIO_INPUT_DIR)
+    assert os.path.isdir(AUDIO_OUTPUT_DIR)
+    assert os.path.isdir(EFFECT_TEMPLATE_DIR)
+    assert os.path.isdir(ANALYSER_DIR)
+    assert os.path.isdir(RAY_RESULTS_DIR)
